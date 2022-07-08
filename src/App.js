6636bf24d7a3e49e6767230dd213484784de2374
Felix Ouma <fatonoh@gmail.com>
2022-07-08 04:15:35 +0300

    Add application navigation menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import BooksContainer from './components/BooksContainer';
+import Categories from './components/Categories';
 
 function App() {
   return (
-    <div className="App">
-      <BooksContainer />
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<BooksContainer />} />
+        <Route path="/books" element={<BooksContainer />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Categories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Categories = () => (
+  <h1 className="book-card d-flex space-between m-1">Coming Soon</h1>
+);
+
+export default Categories;
