e48ea404bc99676b34c4cd78b45dd63f6822b3cc
Felix Ouma <fatonoh@gmail.com>
2022-07-12 02:05:13 +0300

    Add react-redux and react-toolkit to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import NavBar from './components/NavBar';
 import BooksContainer from './components/BooksContainer';
 import Categories from './components/Categories';
@@ -7,12 +9,14 @@ import Categories from './components/Categories';
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<BooksContainer />} />
-        <Route path="/books" element={<BooksContainer />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
+      <Provider store={store}>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<BooksContainer />} />
+          <Route path="/books" element={<BooksContainer />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Provider>
     </BrowserRouter>
   );
 }
